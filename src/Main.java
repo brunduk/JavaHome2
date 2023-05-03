3f0ced49a0c84d5512c90a77f59edcc26396aae0
double d <brunduk4415@gmail.com>
2023-05-04 00:24:13 +0300

    work complite

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        }
+        int ticket = 7897;
+        int bonus = 20;
+
+        int miles = ticket / bonus;
+
+        System.out.println("Бонусных Миль: " + miles);
     }
+}
